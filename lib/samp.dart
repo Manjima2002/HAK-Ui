c8812cfef9c180ed051af7f6ba1c601f597ca19c
manjima <manjima3890@gmail.com>
2024-04-08 21:31:35 +0530

    samp

---
+++ lib/samp.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Samp extends StatefulWidget {
+  const Samp({super.key});
+
+  @override
+  State<Samp> createState() => _sample();
+}
+
+class _sample extends State<Samp> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+    margin: const EdgeInsets.all(10.0),
+    color: Colors.amber[600],
+    width: 48.0,
+    height: 48.0,
+  ),
+          ],
+            ),
+        ),
+    );
+  }
+  
+}
