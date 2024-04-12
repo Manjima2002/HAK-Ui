0d3bb0ed8c0cc7be2189321150cce0224ca0f0db
manjima <manjimamanju85@gmail.com>
2024-04-12 19:48:41 +0530

    arrivals

--- lib/features/homescreen/home_page.dart
+++ lib/features/homescreen/home_page.dart
@@ -26,7 +26,7 @@ class _MyWidgetState extends State<Homepage> {
           Categories(),
           HomeCarousel(),
           NewArrivelText(),
-          ArrivalITems()
+          ArrivalITems(),
         ],
       ),
     );
