631d7d29a0b98c6ede08035f0b5c9ec794cf3be3
manjima <manjimamanju85@gmail.com>
2024-04-13 23:15:21 +0530

    combo

---
+++ lib/features/homescreen/widgets/combo.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/features/homescreen/widgets/home_carosel.dart';
+
+class Combo extends StatefulWidget {
+  const Combo({super.key});
+
+  @override
+  State<Combo> createState() => _ComboState();
+}
+
+class _ComboState extends State<Combo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    
+     body:Padding(
+      padding:  EdgeInsets.only(top: 30),
+     ),
+    );
+
+    
+  }
+}
